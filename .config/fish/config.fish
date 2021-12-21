a33c4f0f26770e87337b65d6ce7edd32f1d9e07e
Eduard Hildebrandt <eduard.hildebrandt@hildebrandt-holding.com>
2021-12-22 00:20:44 +0100

    changed path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set PATH $HOME/Library/Python/3.8/bin $PATH
+
 oh-my-posh --init --shell fish --config ~/.poshthemes/m365princess.json | source
