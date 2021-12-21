cabcd80f3390f4c509fbf05532e0eff04672fed7
Eduard Hildebrandt <eduard.hildebrandt@hildebrandt-holding.com>
2021-12-21 22:19:29 +0100

    added plugins for hyper

--- .hyper.js
+++ .hyper.js
@@ -133,7 +133,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ["hyper-dracula"],
+    plugins: ["hyper-dracula", "hyperpower", "hyperborder", "gitrocket", "space-pull"],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
