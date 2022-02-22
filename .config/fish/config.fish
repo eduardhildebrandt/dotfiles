d41be3969ce9d40ba64fed97629bb77cde462ad2
Eduard Hildebrandt <eduard.hildebrandt@hildebrandt-holding.com>
2022-02-22 08:33:15 +0100

    testing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,13 @@ end
 
 set PATH $HOME/Library/Python/3.8/bin $PATH
 
+alias ls="exa -lagh --icons --git --time-style=long-iso"
+alias cat="bat"
+alias grep="batgrep"
+alias man="batman"
+alias ping="gping"
+
+fzf_configure_bindings --directory=\cd --git_log=\cl --git_status==\cs --history=\ch --variables=\cv --processes=\cp
+
 oh-my-posh --init --shell fish --config ~/.poshthemes/m365princess.json | source
+thefuck --alias | source
